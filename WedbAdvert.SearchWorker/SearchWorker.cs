39f26589d21ce6dddf55b828ca68b07514313e97
aussiearef <arefkr@gmail.com>
2018-12-31 00:09:02 -0800

    Initial add

---
+++ WedbAdvert.SearchWorker/SearchWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+
+[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace WedbAdvert.SearchWorker
+{
+    public class SearchWorker
+    {
+        public void Function(SNSEvent snsEvent, ILambdaContext context)
+        {
+            foreach (var record in snsEvent.Records)
+            {
+                context.Logger.LogLine(record.Sns.Message);
+            }
+        }
+    }
+}
